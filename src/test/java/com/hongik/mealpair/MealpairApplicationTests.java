de2f07e71c38bfbfced4f4b88e853b4948b54f00
gtaeu <gtaeu1201@gmail.com>
2026-03-27 23:02:10 +0900

    git ignore

---
+++ src/test/java/com/hongik/mealpair/MealpairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongik.mealpair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealpairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
